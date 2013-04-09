5163007fc3581692ab835dea063275f6d81dbca3
Ax <ax@teach>
2013-04-09 02:09:35 +0200

    Matlab file restyled

--- SGHStats.m
+++ SGHStats.m
@@ -1,8 +1,9 @@
-%function [ HeaterOnTime ] = SGHStats( file )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%   SmartGreenHouse stat parser
 
+close all, clear all
+%file='../SmartGreenHouse_data/last/TotTemp';
 file='../SmartGreenHouse_data/last/Temp18';
+
 data=load(file);
 
 fantocmph=16/(0.589*255);
@@ -24,79 +25,96 @@ hcable=data(pstart:pend,5);
 fhumidity=data(pstart:pend,8);
 outf=data(pstart:pend,9);
 
-%%%Caos
-figure('Name','Temperature evolution','NumberTitle','off')
-plot(time,temp,'g',time,heat.*10+15,'r',time,outf./20+15,'b')
-legend('Temperature', 'Heater', 'Fan');
+%%%General state evolution-------------------------------------------------
+figure('Name','Sistem evolution','NumberTitle','off')
+axis(1)=subplot(2,1,1);
+plot(time,temp,'g',time,heat.*10+15,'r*')
+legend('Temperature', 'Heater');
 xlabel('Time (h)')
-
-
-figure('Name','Humidity evol','NumberTitle','off')
-plot(time,humidity./100,'g',time,humidifier,'r',time,outf./255,'b')
+axis(2)=subplot(2,1,2);
+plot(time,humidity./100,'g',time,humidifier,'b*',time,outf./255,'r')
 legend('Humidity', 'Humidifier', 'Fan');
 xlabel('Time (h)')
+linkaxes(axis, 'x');
 
-%%%Hourly statistics
-hactive=sum(heat)/pend*100;
-i=ceil(pstart/3600);
-hhactive=0;
-while i*3600+pstart<pend
-    datawindow=((i-1)*3600:i*3600)+pstart;
-    hhactive(i)=sum(heat(datawindow))/3600;
-    outfhmq(i)=sum(outf(datawindow))/3600*fantocmph;
-    htemp(i,1)=min(temp(datawindow));
-    htemp(i,3)=max(temp(datawindow));
-    htemp(i,2)=sum(temp(datawindow))/3600;
-    hhumidity(i)=sum(humidity(datawindow))/3600;
-    hhcable(i)=sum(hcable(datawindow))/3600;
+if(max(time)<12)
+    Tmedian=median(temp)
+    Hmedian=median(humidity)
+else
     
-    i=i+1;
+    %%%Hourly statistics (Only if more than 12 hour)-----------------------
+    hactive=sum(heat)/pend*100;
+    i=ceil(pstart/3600);
+    hhactive=0;
+    while i*3600+pstart<pend
+        datawindow=((i-1)*3600:i*3600)+pstart;
+        hhactive(i)=sum(heat(datawindow))/3600;
+        outfhmq(i)=sum(outf(datawindow))/3600*fantocmph;
+        htemp(i,1)=min(temp(datawindow));
+        htemp(i,3)=max(temp(datawindow));
+        htemp(i,2)=sum(temp(datawindow))/3600;
+        hhumidity(i)=sum(humidity(datawindow))/3600;
+        hhcable(i)=sum(hcable(datawindow))/3600;
+        
+        i=i+1;
+    end
+    htime=1:(i-1);
+    figure('Name','Heater Hourly Statistic','NumberTitle','off')
+    subplot(2,1,1)
+    bar(hhactive,'r')
+    title('Heater active')
+    xlabel('Time (h)')
+    ylabel('%')
+    subplot(2,1,2)
+    bar(hhcable)
+    title('Heat cable on time')
+    xlabel('Time (h)')
+    ylabel('%')
+    
+    figure('Name','Other actuators Hourly Statistic','NumberTitle','off')
+    subplot(2,1,1)
+    bar(hhumidity)
+    title('Humidifier Active')
+    xlabel('Time (h)')
+    ylabel('%')
+    subplot(2,1,2)
+    bar(outfhmq)
+    title('Airflow')
+    xlabel('Time (h)')
+    ylabel('m^3/h')
+    
+    figure('Name','State Variables','NumberTitle','off')
+    axis(1)=subplot(2,1,1);
+    %bar(htemp)
+    plot(htime,htemp(:,1),'b*',htime,htemp(:,2),'g*',htime,htemp(:,3),'r*')
+    title('Temperature')
+    xlabel('Time (h)')
+    ylabel('°C')
+    legend('Min','Med','Max')
+    axis(2)=subplot(2,1,2);
+    bar(hhumidity)
+    title('Humidity median')
+    xlabel('Time (h)')
+    ylabel('%')
+    linkaxes(axis, 'x');
+    
+    if(max(time)/24>3)
+        %%Daily Stat (If more than 3 days)-------------------------------
+        i=1;dhactive=0;
+        while i*24<length(hhactive)
+            datawindow=(i-1)*24+1:i*24;
+            dhactive(i)=sum(hhactive(datawindow));
+            i=i+1;
+        end
+        figure('Name','Daily Heater On Time','NumberTitle','off')
+        xlabel('Days')
+        ylabel('%')
+        bar(dhactive)
+    end
+    %Text statistic
+    HeaterOnTime=sum(htemp(:,2))/100
+    
 end
-htime=1:(i-1);
-figure('Name','Heater Hourly Statistic','NumberTitle','off')
-subplot(2,1,1)
-bar(hhactive,'r')
-title('Heater active percentage')
-subplot(2,1,2)
-bar(hhcable)
-title('Heat cable on time')
-
-figure('Name','Other actuators Hourly Statistic','NumberTitle','off')
-subplot(2,1,1)
-bar(hhumidity)
-title('Humidifier active percentage')
-subplot(2,1,2)
-bar(outfhmq)
-title('Airflow m^3/h')
-
-figure('Name','State Variables','NumberTitle','off')
-subplot(2,1,1)
-%bar(htemp)
-plot(htime,htemp(:,1),'b*',htime,htemp(:,2),'g*',htime,htemp(:,3),'r*')
-title('Temperature')
-subplot(2,1,2)
-bar(hhumidity)
-title('Humidity median')
-
-htime=0;
-hdc=0;
-hi=1;
-start=0;
-
-%%Daily-------------------------------
-i=1;dhactive=0;
-while i*24<length(hhactive)
-    datawindow=(i-1)*24+1:i*24;
-dhactive(i)=sum(hhactive(datawindow));
-i=i+1;
-end
-figure('Name','Daily Heater On Time','NumberTitle','off')
-bar(dhactive)
-
-%Text statistic
-HeaterOnTime=sum(htemp(:,2))/100
-
-
 %%Data
 %1. temp
 %2. heatercl
