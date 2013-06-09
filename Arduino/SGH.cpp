a94d0e2b1e5fafd25a267fed79a1c6631208d5e6
Ax <ax@teach>
2013-06-09 22:40:32 +0200

    Actuators fully integrated in SGH

--- Arduino/SGH.cpp
+++ Arduino/SGH.cpp
@@ -13,7 +13,17 @@
 
 #define GOODLEVEL HIGH
 
-///------------------------------------------------
+///Actuators Defines------------------------------------------------
+#define HEATPIN 2
+#define HUMPIN 3
+#define HEATCABLEPIN 4
+#define OUTFPIN   10
+#define FANT 25
+
+#define HUMCONTROLLED
+
+#define HCDELTA 4
+
 ///Modular methods
 /** @brief update
   *
@@ -47,12 +57,70 @@ int SGH::updateSTD()
     //Read Light sensor----------------------------------------------------
     this->state.light=analogRead(LIGHTAPIN);
     //Filtered variables
-    //this->state.flight=this->lplight.update(state->light);
-    //this->state.fhumidity=this->lphumidity.update(state->humidity);
+    //this->state.flight=this->lplight.update(state.light);
+    //this->state.fhumidity=this->lphumidity.update(state.humidity);
+///Actuators updating--------------------------------------------------------------------------
+  int ret;
+  state.eactuators=0;
+  //-----------------------------------------------------------------------------
+  //Verify DHT11 state--------------------------------------------------------------------------------
+  //If sensor major error occurred
+  if(state.esensors&State::ESENS_DHTERR)
+  {
+    ///Emergency suspend function=================================================================
+    //Shut down both heater and humidifier
+    this->heater.forceOff();
+    this->heatcable.forceOff();
+    this->humidifier.forceOff();
+    //Stop Fan
+    this->outFan.stop();
+    //Add log
+    state.log(State::CRITICAL,"DHT Major error. Shutting down all actuators");
+    ///=============================================================================================
+  }
+  //----------------------------------------------------------------------------------------------------
+  else
+  {
+    //Heater------------------------------------------------------------------------------------------------
+    bool dbgctrl=t.update(state.temp);
+    //Serial.print("H:");
+    //Serial.println(dbgctrl);
+    state.heater = this->heater.set(dbgctrl);
+
+    //Heat Cable------------------------------------------------------
+    state.heaterCable=this->heatcable.set(tc.update(state.temp));
+
+    //Humidifier------------------------------------------------------
+#ifdef HUMCONTROLLED
+    //If water level is not good shut down humidifier
+    if(state.level==0)
+      state.humidifier=this->humidifier.off();
+    //or evaluate status
+    else
+      state.humidifier=this->humidifier.set(h.update(state.humidity));
+
+#endif
+  }
+
+    //Full power if > 30°
+  if(state.temp>30)
+    state.outFan=outFan.setSpeed(255);
+  else
+  {
+    //Evaluate distance from higher point
+    float ferror=state.temp-FANT;
+    float kc=30;
+
+    if(ferror>0)
+      state.outFan=outFan.setSpeed(kc*ferror);
+    else
+      state.outFan=outFan.setSpeed(0);
+  }
 ///-----------------------------------------------------------------------------------------------------
-  act.update(&this->state);
+
   //SD Log -------------------------------------------
   this->state.saveStats();
+
 }
 
 /** @brief init
@@ -73,7 +141,33 @@ Serial.begin(9600); // only required for testing
     //this->lphumidity.setup(HUMIDITYTAU);
 
 ///Actuators---------------------------------------------------------------------------------------------
-  act.setup();
+ ///MaxOn, MinOn, MaxOff,MinOff
+  this->heater.init(HEATPIN,300,60,0,60); //Heater setup
+  this->humidifier.init(HUMPIN,0,60,0,60);  //Humidifier pin
+  this->heatcable.init(HEATCABLEPIN,0,60,0,0);
+
+  //Fan 27 mq/h
+  //1 mq/20min = 30 pwm ->
+  //Simple temperature control
+
+  outFan.setup(OUTFPIN,45,255,600,18000);
+
+///Parameter definition
+///Heater 18°+-3
+///Humidifier 35+-10
+///Heat cable 25+-5
+  t.setup(18,6,1);//Set temperature maximum variation around target
+
+#ifdef HUMCONTROLLED
+  //Controlled  humidifier version
+  h.setup(35,20,1);
+#else
+  //Timered Humidifier version
+  cth=ControlTimer(600000,240000);
+#endif
+  //Heat cable
+  tc.setup(25,10,1);
+
   Serial.println("DONE!");
 }
 
--- Arduino/SGH.h
+++ Arduino/SGH.h
@@ -2,8 +2,10 @@
 #define SGH_H
 
 #include <DHT.h>
-#include "Actuators.h"
-
+#include "State.h"
+#include "DigitalOut.h"
+#include "Comparator.h"
+#include "Fan.h"
 //Saved defines---------------
 #define DHTMAXERRN 5
 
@@ -13,6 +15,7 @@ class SGH
         SGH();
         int initSTD(); //Inizialize with standard values
         int updateSTD();
+
         ~SGH();
 ///Public methods----------------------------------------
         int update(); //Update system according to configured modules
@@ -25,13 +28,19 @@ class SGH
         int logInit(uint8_t sdPin); //Initialize sd & logging on defined pin
 ///------------------------------------------------------
     protected:
-    Actuators act;
 State state;
     private:
 
     ///Sensors variables----------------
     DHT dht;
     unsigned short dhterrN;
+    ///Actuators Variables--------------
+         DigitalOut heater,
+    humidifier,
+    heatcable;
+    Comparator t,tc;
+    Comparator h;
+    Fan outFan;
 };
 
 #endif // SGH_H
