eeb669306d719e4eaa1edabddbe174eaee0c52af
Ax <ax@teach>
2013-06-16 18:54:21 +0200

    License

--- Arduino/LowPassFilter.h
+++ Arduino/LowPassFilter.h
@@ -1,3 +1,15 @@
+/*******************************************************************
+*                   ==SmartGreenHouse==
+*   Low Pass Filter
+*   Created: 17/02/2013
+*   Author:  Ax
+ *   License: CC BY-SA 3.0
+ *            http://creativecommons.org/licenses/by-sa/3.0/
+ *=====================================================================
+Fan output.
+Re-map fan power between minimum and maximum command.
+Implements an air circle routine.
+********************************************************************/
 #ifndef LOWPASSFILTER_H
 #define LOWPASSFILTER_H
 
