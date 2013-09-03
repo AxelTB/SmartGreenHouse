908e03ea862eb37d8878ea875e1c98aaf2d8574e
Ax <ax@teach>
2013-09-03 02:06:03 +0200

    Analog loop stuff created

---
+++ Arduino/AnalogLoop.h
@@ -0,0 +1,18 @@
+#ifndef ANALOGLOOP_H
+#define ANALOGLOOP_H
+
+#include "Loop.h"
+#include "AnalogOut.h"
+#include "SGHPID.h"
+class AnalogLoop : public Loop
+{
+    public:
+        /** Default constructor */
+        AnalogLoop();
+        int operator<<(AnalogOut);
+        int operator<<(SGHPID);
+    protected:
+    private:
+};
+
+#endif // ANALOGLOOP_H
---
+++ Arduino/SGHPID.h
@@ -0,0 +1,22 @@
+#ifndef SGHPID_H
+#define SGHPID_H
+
+#include "TimeCounter.h"
+class SGHPID
+{
+    public:
+        /** Default constructor */
+        SGHPID();
+        SGHPID(double kp,double ki,double kd,double maxI=1500);
+
+        void set(double kp,double ki,double kd,double maxI=1500);
+        int update(double error);
+        /** Default destructor */
+        virtual ~SGHPID();
+    protected:
+    TimeCounter tc;
+    double lastErr,sumErr;
+    private:
+};
+
+#endif // SGHPID_H
